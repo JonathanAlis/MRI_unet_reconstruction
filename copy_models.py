43c32c2f599925d3293280ecc98954b8eb2df71f
jonathanalis <jonathanalis@gmail.com>
2024-01-15 08:38:11 -0300

    script pra copiar modelos..

--- copy_models.py
+++ copy_models.py
@@ -2,14 +2,16 @@ import pandas as pd
 import os
 import shutil
 
-os.mkdir('models_paramaters')
+if not os.path.exists('models_paramaters'):
+    os.mkdir('models_paramaters')
 rad_lines = [20,40,60,80,100]
 
 
 for rl in rad_lines:
-    df=pd.read_excel(f'results_{rl}lines.xlsx')
+    df=pd.read_csv(f'results/results_{rl}lines.csv')
+    print(df)
     for i, row in df.iterrows():
         path = row['model_path']
         filename = os.path.basename(path)
-        shutil.copy(row, f'models_paramaters/{filename}')
+        shutil.copy(path, f'models_paramaters/{filename}')
         
